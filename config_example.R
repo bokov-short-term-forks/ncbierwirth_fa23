fc17028bbd065f688e1281721177014c4921237a
Alex Bokov <bokov@uthscsa.edu>
2023-12-06 09:34:04 -0600

    Instructor feedback on data.R and related files (adding config_example.R)

---
+++ config_example.R
@@ -0,0 +1,5 @@
+# copy this file to config_local.R in order to override the default values in data.R with the values set here
+authemail <- 'ncbierwirth@gmail.com'
+projectid <- 'iron-atom-401719'
+datasetid <- 'ICU_Admissions_Data'
+
